e9c45eeeb065ffa4e1ca2da9094d87fd43cbf95a
Oscar Hernandez <98Hernandez.Oscar@gmail.com>
2022-12-10 00:56:10 -0900

    time steps

--- simulate.py
+++ simulate.py
@@ -1,5 +1,10 @@
 
 import pybullet as p
+from time import sleep
 
 physicsClient = p.connect(p.GUI)
+for i in range(1,1000):
+    print(i)
+    p.stepSimulation()
+    sleep(1/100)    
 p.disconnect()
