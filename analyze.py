2558aca95303917ed3408b263cf2f66bad843fb2
Oscar Hernandez <98Hernandez.Oscar@gmail.com>
2022-12-12 23:33:20 -0900

    G. move forward with motors

--- analyze.py
+++ analyze.py
@@ -4,8 +4,15 @@ import numpy as np
 
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
 frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
-plt.plot(backLegSensorValues, linewidth=2)
-plt.plot(frontLegSensorValues, linewidth=5)
-plt.legend("back", "front")
+BL_targetAngles = np.load("data/BL_targetAngles.npy")
+FL_targetAngles = np.load("data/FL_targetAngles.npy")
+
+#plt.plot(backLegSensorValues, linewidth=2)
+#plt.plot(frontLegSensorValues, linewidth=5)
+#plt.legend("back", "front")
+
+plt.plot(BL_targetAngles, linewidth=5)
+plt.plot(FL_targetAngles, linewidth=3)
+plt.legend(["backLeg Target Angles", "frontLeg Target Angles"])
 
 plt.show()
--- simulate.py
+++ simulate.py
@@ -7,6 +7,9 @@ import pyrosim.pyrosim as pyrosim
 import random
 from time import sleep
 
+BL = {"amplitude": pi/6, "frequency": 10.0, "phaseOffset": pi/4.0}
+FL = {"amplitude": pi/4, "frequency": 10.0, "phaseOffset": 0.0}
+
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.8)
@@ -19,8 +22,11 @@ pyrosim.Prepare_To_Simulate(robotId)
 num = 1000
 backLegSensorValues = np.zeros(num)
 frontLegSensorValues = np.zeros(num)
+t = np.linspace(0, 2*pi, num)
+BL["targetAngles"] = BL["amplitude"] * np.sin(BL["frequency"] * t + BL["phaseOffset"])
+FL["targetAngles"] = FL["amplitude"] * np.sin(FL["frequency"] * t + FL["phaseOffset"])
 
-MAX_FORCE = 25
+MAX_FORCE = 75
 
 for i in range(1, num):
     p.stepSimulation()
@@ -31,23 +37,27 @@ for i in range(1, num):
         bodyIndex = robotId,
         jointName = "Torso_BackLeg",
         controlMode = p.POSITION_CONTROL,
-        targetPosition = random.random()*pi-pi/2,
+        targetPosition = BL["targetAngles"][i],
         maxForce = MAX_FORCE
     )
     pyrosim.Set_Motor_For_Joint(
         bodyIndex = robotId,
         jointName = "Torso_FrontLeg",
         controlMode = p.POSITION_CONTROL,
-        targetPosition = random.random()*pi-pi/2,
+        targetPosition = FL["targetAngles"][i],
         maxForce = MAX_FORCE
     )
 
-    print(i, backLegSensorValues[i], frontLegSensorValues[i])
-    sleep(1/60)   
+    print(i, backLegSensorValues[i], frontLegSensorValues[i], BL["targetAngles"][i])
+    sleep(1/240)   
 
 with open('./data/backLegSensorValues.npy', 'wb') as fh:
     np.save(fh, backLegSensorValues)
 with open('./data/frontLegSensorValues.npy', 'wb') as fh:
     np.save(fh, frontLegSensorValues)
+with open('./data/BL_targetAngles.npy', 'wb') as fh:
+    np.save(fh, BL["targetAngles"])
+with open('./data/FL_targetAngles.npy', 'wb') as fh:
+    np.save(fh, FL["targetAngles"])
 
 p.disconnect()
