becf4ac2f2e44551f3899527926671358894072c
Oscar Hernandez <98Hernandez.Oscar@gmail.com>
2022-12-12 17:15:44 -0900

    simulate one link

--- simulate.py
+++ simulate.py
@@ -3,8 +3,11 @@ import pybullet as p
 from time import sleep
 
 physicsClient = p.connect(p.GUI)
+
+p.loadSDF("box.sdf")
 for i in range(1,1000):
     print(i)
     p.stepSimulation()
-    sleep(1/60)    
+    sleep(1/70)    
+
 p.disconnect()
