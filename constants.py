65effa8c74fa9fcea077719812db47b83d5ca73a
Oscar Hernandez <98Hernandez.Oscar@gmail.com>
2022-12-20 09:40:33 -0900

    N. neural network parametrization

--- constants.py
+++ constants.py
@@ -4,9 +4,12 @@ from math import pi
 G = -9.8
 MAX_FORCE = 50
 
+numSensorNeurons = 3
+numMotorNeurons = 2
+
 numTimeSteps = 1000
 T = 2 * pi
 waitTime = 1 / 360
 
-numberOfGenerations = 9
-populationSize = 9
+numberOfGenerations = 1
+populationSize = 1
--- solution.py
+++ solution.py
@@ -4,6 +4,7 @@ from os import system, path
 from random import randint, random
 from time import sleep
 
+import constants as c
 import pyrosim.pyrosim as pyrosim
 
 length = 1
@@ -21,12 +22,12 @@ class SOLUTION():
     def Set_ID(self, ID):
         self.myID = ID
 
-    def Send_World(self):
+    def Generate_World(self):
         pyrosim.Start_SDF("world.sdf")
         pyrosim.Send_Cube(name="Box", pos=[x-length*2,y+width*2,z], size=[length,width,height])
         pyrosim.End()
     
-    def Send_Body(self):
+    def Generate_Body(self):
         pyrosim.Start_URDF("body.urdf")
         pyrosim.Send_Cube(name="Torso", pos=[0,0.0,1.5], size=[length,width,height])
         pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[-0.5,0,1.0])
@@ -35,22 +36,22 @@ class SOLUTION():
         pyrosim.Send_Cube(name="BackLeg", pos=[0.5,0,-0.5], size=[length,width,height])
         pyrosim.End()
     
-    def Send_Brain(self):
+    def Generate_Brain(self):
         pyrosim.Start_NeuralNetwork("brain" + str(self.myID) + ".nndf")
         pyrosim.Send_Sensor_Neuron(name=0, linkName="Torso")
         pyrosim.Send_Sensor_Neuron(name=1, linkName="BackLeg")
         pyrosim.Send_Sensor_Neuron(name=2, linkName="FrontLeg")
         pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
         pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
-        for currentRow in range(0,3):
-            for currentColumn in range(0,2):
-                pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn+3, weight=self.weights[currentRow][currentColumn])
+        for currentRow in range(0, c.numSensorNeurons):
+            for currentColumn in range(0, c.numMotorNeurons):
+                pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn+c.numSensorNeurons, weight=self.weights[currentRow][currentColumn])
         pyrosim.End()
 
     def Start_Simulation(self, directOrGUI):
-        self.Send_World()
-        self.Send_Body()
-        self.Send_Brain()
+        self.Generate_World()
+        self.Generate_Body()
+        self.Generate_Brain()
         system("python3 simulate.py " + directOrGUI + " " + str(self.myID) + " 2&>1 & ")
 
     def Wait_For_Simulation_To_End(self):
